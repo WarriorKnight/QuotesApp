393ed824b394f9797f2e7cd0ef8ee2a9a4c2c586
Darkk <vojtba@gmail.com>
2025-05-06 14:02:09 +0200

    add confige service for database uri

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { QuoteModule } from './quote/quote.module';
+import { AuthorModule } from './author/author.module';
+import { TagModule } from './tag/tag.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    QuoteModule,
+    AuthorModule,
+    TagModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
